fe75afddf46a9d1708b1d6c5c058f040477df55f
Zach <itsaboy3366@gmail.com>
2024-05-25 14:25:44 -0400

    small tweak to code snippet formats

--- client/src/components/HomePage/ReactSnippet.jsx
+++ client/src/components/HomePage/ReactSnippet.jsx
@@ -3,18 +3,18 @@ export default function ReactSnippet() {
     <pre>
       <code>
         {`import { useState } from "react";
-  import { Outlet } from "react-router-dom";
-  import "./App.css";
+import { Outlet } from "react-router-dom";
+import "./App.css";
   
-  export default function App() {
-    const [state, setState] = useState();
-  
-    return (
-      <>
-        <Outlet />
-      </>
-    );
-  }`}
+export default function App() {
+  const [state, setState] = useState();
+
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}`}
       </code>
     </pre>
   );
