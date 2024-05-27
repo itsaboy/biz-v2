c45d8d609f9a494635946b23da308939c3c3079b
Zach <itsaboy3366@gmail.com>
2024-05-26 20:42:18 -0400

    fixed build process bug

--- client/src/layouts/BodyLayout.jsx
+++ client/src/layouts/BodyLayout.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
-import BGAnim from "../components/BGAnim";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import FloatingNav from "../components/FloatingNav";
+import background from "../assets/background.svg";
 
 export default function BodyLayout() {
   return (
     <div className="relative flex flex-col min-h-screen h-full">
-      <BGAnim />
+      <div
+        className="absolute inset-0 bg-no-repeat bg-cover bg-center bg-fixed -z-10"
+        style={{
+          backgroundImage: `url("${background}")`,
+        }}
+      />
       <Header />
       <main className="relative flex-grow">
         <Outlet />
